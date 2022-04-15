7ab8e03d747fa9c93684911272e45b8152ebe30b
merlinjones10 <78985319+merlinjones10@users.noreply.github.com>
2022-04-15 22:29:52 +0100

    cmts removed

--- node-content/cryptoDevice.js
+++ node-content/cryptoDevice.js
@@ -18,9 +18,6 @@ Max.addHandler('get', (coin,days) => {
       });
 	  Max.post(prices);
       Max.outlet(prices);
-
-	
-
     })
     .catch(function (error) {
       Max.post('error:');
@@ -31,7 +28,7 @@ Max.addHandler('get', (coin,days) => {
     });
 });
 
-// node tester (in console)
+// node tester (without nodeformax to test in console)
 // const nodeTester = (coin) => {
 //   axios
 //     .get(`${baseUrl}/${coin}/market_chart?vs_currency=usd&days=6&interval=daily`)
@@ -39,7 +36,6 @@ Max.addHandler('get', (coin,days) => {
 //       myResponse = JSON.stringify(response.data);
 //       let prices = response.data.prices.map((price) => {
 //         return Math.round(price[1] * 100) / 100
-//
 //       });
 //       console.log(prices);
 //     })
@@ -52,93 +48,4 @@ Max.addHandler('get', (coin,days) => {
 ///
 // nodeTester()
 
-// const axios = require('axios');
-// const Max = require('max-api');
-// getUrl =
-//   'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=6&interval=daily';
-// let myResponse;
-// Max.post('Loaded the script');
 
-// Max.addHandler('get', () => {
-//   axios
-//     .get(getUrl)
-//     .then(function (response) {
-//       // myResponse = JSON.stringify(response.data);
-//       let prices = response.data.prices.map((price) => {
-//         return price[1];
-//       });
-//       Max.post(prices);
-//       Max.outlet(prices);
-//     })
-//     .catch(function (error) {
-//       Max.post('error');
-//     })
-//     .then(function () {
-//       Max.post('END');
-//     });
-// });
-
-// // node tester (in console)
-// const nodeTester = () => {
-//   axios
-//     .get(getUrl)
-//     .then(function (response) {
-//       myResponse = JSON.stringify(response.data);
-//       let prices = response.data.prices.map((price) => {
-//         return price[1];
-//       });
-//       console.log(prices);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     })
-//     .then(function () {});
-// };
-
-///
-
-
-// const axios = require('axios');
-// const Max = require('max-api');
-// getUrl =
-//   'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=6&interval=daily';
-// let myResponse;
-// Max.post('Loaded the script');
-
-// Max.addHandler('get', () => {
-//   axios
-//     .get(getUrl)
-//     .then(function (response) {
-//       // myResponse = JSON.stringify(response.data);
-//       let prices = response.data.prices.map((price) => {
-//         return price[1];
-//       });
-//       Max.post(prices);
-//       Max.outlet(prices);
-//     })
-//     .catch(function (error) {
-//       Max.post('error');
-//     })
-//     .then(function () {
-//       Max.post('END');
-//     });
-// });
-
-// // node tester (in console)
-// const nodeTester = () => {
-//   axios
-//     .get(getUrl)
-//     .then(function (response) {
-//       myResponse = JSON.stringify(response.data);
-//       let prices = response.data.prices.map((price) => {
-//         return price[1];
-//       });
-//       console.log(prices);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     })
-//     .then(function () {});
-// };
-
-// ///
